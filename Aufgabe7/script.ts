9f0a0ba2ee502473347eb9443166d14bba9aa5e4
TobMer <48830884+TobMer@users.noreply.github.com>
2019-05-18 17:05:36 +0200

    JS

---
+++ Aufgabe7/script.ts
@@ -0,0 +1,36 @@
+window.alert("Button geklickt?!"); //Das löst eine Meldung aus
+console.log('Test1');
+//Javascript und Typescript geht los
+
+window.onload = function () {  //Lässt in Konsole Text erscheinen
+
+
+    console.log("Objekte wurden geladen");//Text in Konsole
+    var name = "tobias"; //Typ String
+    var surname = "merkle"; 
+    
+    var number1 = 1;//Typ Number
+    var number2 = 2;
+    var number3 = 24;
+    
+    
+    number1 = 2; //Neue Wertzuweisung
+    console.log(number1 +number1);
+   console.log(name + surname); 
+   console.log(number1 + number2 +number3);//Addiert 
+    
+    
+}
+
+
+//Button Event
+document.getElementById("btn").addEventListener("click", function(){
+    document.getElementById("demo").innerHTML = "Was geht ab";
+});
+
+
+
+
+
+
+
