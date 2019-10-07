06da2f0892241147ee852d2eae9f523a0b16ebf2
TobMer <48830884+TobMer@users.noreply.github.com>
2019-10-07 23:05:15 +0200

    erased

--- TUTORIUM/übung.js
+++
@@ -1,5 +0,0 @@
-window.onload = function () {
-    window.alert("Bekämpfe die Monster");
-    console.log("Website fertig geladen");
-};
-//# sourceMappingURL=übung.js.map
--- Typescript/Übung.ts
+++
@@ -1,7 +0,0 @@
-
-/// ARRAY
-
-
-
-let s = {"zahl": 7, "wahr": true, text: "Hallo"}
-s[4] = [101, 102]
